3812f9336cdc76daa0d4651a0b7294fe787ad084
deborahdelimaa <deborahftlima@gmail.com>
2023-02-09 16:43:58 +0000

    done

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 const mongoose = require('mongoose');
 
+
 // Import of the model Recipe from './models/Recipe.model.js'
 const Recipe = require('./models/Recipe.model');
 // Import of the data from './data.json'
 const data = require('./data');
+const { findOneAndUpdate } = require('./models/Recipe.model');
 
 const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
 
 //Method 1 : Using Async Await
+
+/* const recipe = mongoose.model("recipe", {title:String}) */
+
+const soup = new Recipe({title:"Beans Soup"});
 
 const manageRecipes = async () => {
   try {
@@ -19,6 +25,18 @@ const manageRecipes = async () => {
     await Recipe.deleteMany();
 
     // Run your code here, after you have insured that the connection was made
+
+  await Recipe.create({title:"Beans Soup", level:"easy peasy", ingredients:["beans"],cuisine:true, dishType:"soup",duration:10, creator:"Sheela Campos"})
+  console.log(soup)
+  await Recipe.insertMany(data)
+  for(let i = 0; i < data.length; i++){
+    console.log(data[i].title)
+  }
+   let rigatoni = await Recipe.findOneAndUpdate({title:"Rigatoni alla Genovese"}, {duration:100}, {returnOriginal:false})
+   console.log("Rigatoni duration was updated!")
+  let cake = await Recipe.deleteOne({title:"Carrot Cake"})
+   console.log("You have removed Carrot Cake Recipe")
+    
   } catch (error) {
     console.log(error);
   }
@@ -42,3 +60,5 @@ manageRecipes();
   .catch((error) => {
     console.error('Error connecting to the database', error);
   }); */
+
+  mongoose.connection.close()
--- models/Recipe.model.js
+++ models/Recipe.model.js
@@ -2,7 +2,43 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const recipeSchema = new Schema({
-  // TODO: write the schema
+title:{
+  type: String,
+  required: true,
+  unique:true
+},
+level: {
+  type: String,
+  lowercase: true,
+  enum: ["easy peasy", "amateur chef", "ultrapro chef"]
+},
+ingredient: {
+  type: [ String ],
+},
+cuisine: {
+  type: String,
+  required: true
+},
+dishType: {
+  type: String,
+  lowercase:true,
+  enum:["breakfast", "main_course", "soup", "snack", "drink", "dessert", "other"]
+},
+image: {
+  type: String,
+  default: "https://images.media-allrecipes.com/images/75131.jpg"
+},
+duration: {
+  type: Number,
+  min:0
+},
+creator:{
+  type: String
+},
+created: {
+  type: Date,
+  default:Date.now
+}
 });
 
 const Recipe = mongoose.model('Recipe', recipeSchema);
